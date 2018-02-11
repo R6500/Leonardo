af1f4449c9192c86a0d1a78310347eab2c39806c
Vicente Jimenez <vic20git@gmail.com>
2018-02-11 16:32:43 +0100

    Add files via upload

---
+++ fastToggleTest.ino
@@ -0,0 +1,37 @@
+// Fast Toggle Test
+
+void setup()
+{
+}
+
+#define BIT(n)                      (1<<n)
+#define SET_FLAG(REGISTER,FLAG)     REGISTER|=(FLAG)
+#define CLEAR_FLAG(REGISTER,FLAG)   REGISTER&=~(FLAG) 
+
+// Toggles pin 10
+// Never returns
+// Gives a frequency of about 43kHz
+//
+void highLevelToggle(void)
+{
+pinMode(10,OUTPUT);
+while (1)
+   digitalWrite(10,!digitalRead(10));
+}
+
+// Toggle pin 10
+// Never returns
+// Gives a frequency of about 2.5MHz
+//
+void lowLevelToggle(void)
+{
+SET_FLAG(DDRB,BIT(6));
+while (1)
+   PINB=bit(6); 
+}
+
+void loop()
+{
+lowLevelToggle();  
+}
+
